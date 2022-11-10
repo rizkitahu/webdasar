89eaf3594d9b71ff9bea13731331ceaa87ae6747
rizkitahu <m.kikiblokf4no8@gmail.com>
2022-11-10 11:36:01 +0700

    perscomite

---
+++ javascript/masuk.js
@@ -0,0 +1,9 @@
+<script>
+let masuk = confirm('Apakah anda ingin melanjutkan?')
+if (masuk) {
+    let nama = prompt('masukkan nama')
+    alert(`selamat datang ${nama}`)
+} else {
+    alert('bye bye')
+}
+</script>
